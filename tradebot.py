a3748eff510037f1b07d0e17ff53e59fd30276e8
sambhavnoobcoder <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-01-27 14:16:33 +0530

    Create tradebot.py
    base bot buying random stocks

---
+++ tradebot.py
@@ -0,0 +1,51 @@
+from lumibot.brokers import Alpaca
+from lumibot.backtesting import YahooDataBacktesting
+from lumibot.strategies.strategy import Strategy
+from lumibot.traders import Trader
+from datetime import datetime 
+from alpaca_trade_api import REST 
+from timedelta import Timedelta 
+from finbert_utils import estimate_sentiment
+
+API_KEY = "PKK8E7A7H43GAESF91R3" 
+API_SECRET = "TsuZrFlwk5NTeg7k6dXnd6qSoP6Ir9xW1wpK6bVA" 
+BASE_URL = "https://paper-api.alpaca.markets"
+
+ALPACA_CREDS = {
+    "API_KEY":API_KEY, 
+    "API_SECRET": API_SECRET, 
+    "PAPER": True
+}
+
+class MLTrader(Strategy): 
+    def initialize(self, symbol:str="SPY", cash_at_risk:float=.5): 
+        self.symbol = symbol
+        self.sleeptime = "24H" 
+        self.last_trade = None 
+        self.cash_at_risk = cash_at_risk
+        self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
+
+    def on_trading_iteration(self):
+        if self.last_trade == None:
+            order = self.create_order(
+            self.symbol, 
+            10, 
+            "buy",
+            type="market"
+            )
+            self. submit_order (order)
+            self.last_trade = "buy"
+
+start_date = datetime(2020,1,1)
+end_date = datetime(2023,12,31) 
+broker = Alpaca(ALPACA_CREDS) 
+strategy = MLTrader(name='mlstrat', broker=broker, 
+                    parameters={"symbol":"SPY", 
+                                "cash_at_risk":.5})
+strategy.backtest(
+    YahooDataBacktesting, 
+    start_date, 
+    end_date, 
+    parameters={"symbol":"SPY", "cash_at_risk":.5}
+)
+
